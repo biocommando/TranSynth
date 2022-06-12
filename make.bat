b5660c46318104bdffa6f1656c969c6be5474a1a
joonas <joonas1000@gmail.com>
2022-06-12 20:44:29 +0300

    Use g++ -shared instead of dllwrap

--- make.bat
+++ make.bat
@@ -5,12 +5,13 @@ echo #define GIT_COMMIT \>> build.h
 git log --format=\""%%h\"" -n 1 >> build.h
 set cxx=g++ -static -c
 
+set flags=-DVST_COMPAT -DWIN32 -DWINDOWS -DUNICODE -DWINDOWS_GUI -D__x86_64__
+
 %cxx% AdsrEnvelope.cpp BasicOscillator.cpp EnvelopeStage.cpp MS20Filter.cpp ^
     ParameterHolder.cpp  PluginParameter.cpp SubSynth.cpp SubSynthParam.cpp ^
     SubSynthParams.cpp SubSynthVoiceManagement.cpp HonestEarRapeFilter.cpp ^
-    VAStateVariableFilter.cpp -Ofast || exit /b
+    VAStateVariableFilter.cpp -Ofast %flags% || exit /b
 
-set flags=-DVST_COMPAT -DWIN32 -DWINDOWS -DUNICODE -DWINDOWS_GUI 
 rem "-DFST_DEBUG_OUTPUT=""C:/users/public/FstLog.txt"""
 %cxx% fst-extension/VSTGui/*.cpp -I"fst-extension/src/" %flags% -trigraphs
 %cxx% fst-extension/src/FstAudioEffect.cpp -I"fst-extension/VSTGui/" -I"fst-extension/src/" %flags%
@@ -19,11 +20,10 @@ rem "-DFST_DEBUG_OUTPUT=""C:/users/public/FstLog.txt"""
 %cxx% TranSynthGui.cpp -I"fst-extension/VSTGui/" -I"fst-extension/src/" %flags% -I"D:\code\js\SimpleScriptCompiler" || exit /b
 %cxx% TranSynth.cpp -I"fst-extension/VSTGui/" -I"fst-extension/src/" %flags% -I"D:\code\js\SimpleScriptCompiler" -Ofast || exit /b
 
-dllwrap  --output-def libTranSynth.def  --driver-name c++ ^
+g++ -shared ^
 AdsrEnvelope.o FstAudioEffect.o BasicOscillator.o EnvelopeStage.o MS20Filter.o ParameterHolder.o ^
 PluginParameter.o SubSynth.o SubSynthParam.o SubSynthParams.o SubSynthVoiceManagement.o TranSynth.o HonestEarRapeFilter.o VAStateVariableFilter.o ^
 aeffguieditor.o TranSynthGui.o vstgui.o vstcontrols.o ScriptCaller.o ^
--L. --add-stdcall-alias -lole32 -lkernel32 -lgdi32 -lgdiplus -luuid -luser32 -lshell32 -mwindows --no-export-all-symbols --def TranSynth.def ^
+-L. -lole32 -lkernel32 -lgdi32 -lgdiplus -luuid -luser32 -lshell32 --def TranSynth.def ^
 -o TransitionSynth.dll -Ofast || exit /b
-
 xcopy TransitionSynth.dll "C:\Program Files\Steinberg\VstPlugins\TransitionSynth.dll" /Y || exit /b
